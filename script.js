bfca86f087d1d78115f5f939c2adc6244cb49297
GrantHollington <grant.j.hollington@gmail.com>
2022-05-15 11:12:51 +1000

    add random number function and data for messages

---
+++ script.js
@@ -0,0 +1,13 @@
+function generateRandomNumber(num) {
+    return Math.floor(Math.random() * num)
+}
+
+// console.log(generateRandomNumber(4))
+
+// message data
+const travelData = {
+    travelLength: ['1 week', '2 weeks', '3 weeks'],
+    destinations: ['New York', 'Tokyo', 'London'],
+    travelCompanion: ['Best friend', 'Mum', 'Dad']
+}
+
