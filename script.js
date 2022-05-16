c2037767a81028d55c4f9dd420532bdf1dbd3fae
GrantHollington <grant.j.hollington@gmail.com>
2022-05-16 16:04:54 +1000

    create switch statement to push message to travelMessage array and create function to console.log random message to terminal

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ const travelData = {
     travelCompanion: ['Best friend', 'Mum', 'Dad']
 }
 
+// store the message in an array
+let travelMessage = [];
+
+// Iterate over the object
+for (let prop in travelData) {
+    let optionNum = generateRandomNumber(travelData[prop].length)
+
+    // create switch statement using the objects properties to customise message added to travel message
+
+    switch(prop) {
+        case 'travelLength' :
+            travelMessage.push(`Your holiday will be for ${travelData[prop][optionNum]}.`)
+            break
+        case 'destinations' :
+            travelMessage.push(`You will travel to ${travelData[prop][optionNum]}`)
+            break
+        case 'travelCompanion' :
+            travelMessage.push(`You will travel with your ${travelData[prop][optionNum]}`)
+            break
+        default:
+            travelMessage.push("You are not travelling.")
+            
+    }
+}
+
+function formatTravel(travel) {
+    const formatted = travelMessage.join('\n')
+    console.log(formatted)
+}
+
+formatTravel(travelMessage)
